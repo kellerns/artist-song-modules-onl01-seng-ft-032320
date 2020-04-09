d56662c641a62c2c2c166379b216b534d971107a
Nicholas Keller <nic.keller328@gmail.com>
2020-04-09 01:26:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,8 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
-
+  include Memorable::InstanceMethods
+  
   @@songs = []
 
   def initialize
